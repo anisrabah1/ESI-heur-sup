ba248b0b49a297939c071bff7c344ed7a9cf32d9
unknown <khaldianisrabah@gmail.com>
2024-04-03 23:34:21 +0100

    tamplate

--- src/tamplate/navbar.js
+++ src/tamplate/navbar.js
@@ -1,7 +1,7 @@
 const navbar = () => {
-    
+
     return ( <div className="container2">
-        <div className="text"><h1>Welcome</h1></div>
+        <div className="text"><h1>welcome</h1></div>
         
         <button className="navbt"></button>
     </div> );
